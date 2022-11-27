98549477250d772c0b01a3289c70ce79338e8239
Ravi <ravivyas922@gmail.com>
2022-11-27 11:53:19 -0500

    commit tree a55280b5290add099093de00e01ff47c85407265

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
+const { AssertionError } = require("chai");
+
 // Code your solution in this file!
+function returnFirstTwoDrivers(arr)
+  {
+    let newArray = arr.slice(0,2);
+    return newArray;
+  };
+  function returnLastTwoDrivers(arr)
+  {
+      let length =arr.length;
+      let newArray = arr.slice(length-2,length);
+      return newArray;
+    };
+const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers];
+
+function createFareMultiplier(num)
+{
+    function fareCalculator(fare)
+    {
+        return fare*num;
+    }
+}
+
+function fareDoubler()
+{
+
+}
+function fareTripler()
+{
+
+}
+function selectDifferentDrivers()
+{
+
+}
